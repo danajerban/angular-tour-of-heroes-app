728c642eacf8803ac4020c465e2ab8334cfa739a
Erban Danaj <danajerban@gmail.com>
2023-05-17 05:23:57 +0200

    v1.0 - Without Part 6 : Getting data from a server

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,63 @@
+/*
+
+*** 1st update ***
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
+
+*** Auto-Generated Version ***
+
+This is what is generated normally by:
+-> 'ng generate module app-routing --flat --module=app'
+
+Notice what we need/don't need and also 2 things:
+
+->'--flat' puts the file in 'src/app' not in its own directory.
+->'--module=app' tells 'ng generate' to register it in the 'imports' array of the 'AppModule'.
+
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
+*/
+
+
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent },
+  { path: 'heroes', component: HeroesComponent }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
